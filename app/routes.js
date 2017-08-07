f9825a4c7bf5c36dbbbb3224c5490db245f8c911
Michael Young <youngmd@149-161-221-222.dhcp-bl.indiana.edu>
2017-08-07 13:18:45 -0400

    fixed default index

--- app/routes.js
+++ app/routes.js
@@ -14,6 +14,6 @@ module.exports = function (app) {
 
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile(__dirname + '/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 };
